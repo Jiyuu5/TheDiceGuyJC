029dd2579ec10051f238052b0ea0c3e3783a9516
Eric Grebe <jiyuu@rockbiter.de>
2024-08-13 10:32:52 +0200

    Add viewModel file and dependencies

---
+++ app/src/main/java/de/rockbiter/thediceguy/ui/SetViewModel.kt
@@ -0,0 +1,25 @@
+package de.rockbiter.thediceguy.ui
+
+import androidx.lifecycle.ViewModel
+import de.rockbiter.thediceguy.data.diceSets
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SetViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(SetUiState())
+    val uiState: StateFlow<SetUiState> = _uiState.asStateFlow()
+
+    init {
+        loadSetPresets()
+    }
+
+    private fun loadSetPresets() {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    activeDiceSet = diceSets[0]
+                )
+            }
+    }
+}
