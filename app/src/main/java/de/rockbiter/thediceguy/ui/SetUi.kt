619d5d66ef63f55447aa97049ee554307cd226d1
Jiyuu5 <jiyuu@rockbiter.de>
2024-08-23 16:12:13 +0200

    change Alert Dialog to Dialog Composable

--- app/src/main/java/de/rockbiter/thediceguy/ui/SetUi.kt
+++ app/src/main/java/de/rockbiter/thediceguy/ui/SetUi.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -27,6 +30,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import de.rockbiter.thediceguy.R
 
@@ -41,16 +45,20 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
             .fillMaxSize()
     ) {
         //Text(text = setUiState.activeDiceSet.name, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 8.dp)) {
-            Text(
-                text = setUiState.scoreAll.toString(), modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                fontSize = 28.sp
-            )
-            if (setUiState.scoreRed > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreGreen > 0 || setUiState.scoreWhite > 0 )){
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+        ) {
+            if (setUiState.activeDiceSet.diceList.isNotEmpty()) {
+                Text(
+                    text = setUiState.scoreAll.toString(), modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    fontSize = 28.sp
+                )
+            }
+            if (setUiState.scoreRed > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreGreen > 0 || setUiState.scoreWhite > 0)) {
                 Text(
                     text = setUiState.scoreRed.toString(),
                     modifier = Modifier
@@ -60,7 +68,7 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                     color = colorResource(id = R.color.red)
                 )
             }
-            if (setUiState.scoreBlue > 0 && (setUiState.scoreRed > 0 || setUiState.scoreGreen > 0 || setUiState.scoreWhite > 0 )){
+            if (setUiState.scoreBlue > 0 && (setUiState.scoreRed > 0 || setUiState.scoreGreen > 0 || setUiState.scoreWhite > 0)) {
                 Text(
                     text = setUiState.scoreBlue.toString(),
                     modifier = Modifier
@@ -70,7 +78,7 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                     color = colorResource(id = R.color.blue)
                 )
             }
-            if (setUiState.scoreGreen > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreRed > 0 || setUiState.scoreWhite > 0 )){
+            if (setUiState.scoreGreen > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreRed > 0 || setUiState.scoreWhite > 0)) {
                 Text(
                     text = setUiState.scoreGreen.toString(),
                     modifier = Modifier
@@ -80,7 +88,7 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                     color = colorResource(id = R.color.green)
                 )
             }
-            if (setUiState.scoreWhite > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreGreen > 0 || setUiState.scoreRed > 0 )){
+            if (setUiState.scoreWhite > 0 && (setUiState.scoreBlue > 0 || setUiState.scoreGreen > 0 || setUiState.scoreRed > 0)) {
                 Text(
                     text = setUiState.scoreWhite.toString(),
                     modifier = Modifier
@@ -89,14 +97,14 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                     fontSize = 24.sp
                 )
             }
-
-
-            IconButton(
-                onClick = { /*TODO*/ }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.more_vert_32),
-                    contentDescription = "Set Menu"
-                )
+            if (setUiState.activeDiceSet.diceList.isNotEmpty()) {
+                IconButton(
+                    onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.more_vert_32),
+                        contentDescription = "Set Menu"
+                    )
+                }
             }
         }
 
@@ -124,15 +132,97 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
             }
         }
     }
+//    if (setUiState.isDiceDialogOpen) {
+//        AlertDialog(
+//            title = { Text(text = "Dice Menu", fontSize = 20.sp) },
+//            text =
+//            {
+//                Column {
+//                    Row {
+//                        IconButton(onClick = {
+//                            setViewModel.addDice(6,"white")
+//                        }) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dice6_3),
+//                                contentDescription = "White D6 Dice"
+//                            )
+//                        }
+//
+//                        IconButton(onClick = {
+//                            setViewModel.addDice(6,"blue")
+//                        }) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dice6_3_blue),
+//                                contentDescription = "Blue D6 Dice"
+//                            )
+//                        }
+//
+//                        IconButton(onClick = {
+//                            setViewModel.addDice(6,"red")
+//                        }) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dice6_3_red),
+//                                contentDescription = "Red D6 Dice"
+//
+//                            )
+//                        }
+//
+//                        IconButton(onClick = {
+//                            setViewModel.addDice(6,"green")
+//                        }) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dice6_3_green),
+//                                contentDescription = "Green D6 Dice"
+//                            )
+//                        }
+//
+//                        IconButton(onClick = {
+//                            setViewModel.addDice(4,"D4_white")
+//                        }) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dice4_3),
+//                                contentDescription = "White D4 Dice"
+//                            )
+//                        }
+//                    }
+//                    Row (
+//                        modifier = Modifier
+//                            .fillMaxWidth(),
+//                        horizontalArrangement = Arrangement.SpaceBetween
+//                    ){
+//                        TextButton(onClick = { setViewModel.clearDiceSet() }) {
+//                            Text(text = "Delete All")
+//                        }
+//                        TextButton(onClick = { setViewModel.closeDiceDialog() }) {
+//                            Text(text = "Dismiss")
+//                        }
+//                    }
+//                }
+//
+//
+//            },
+//            onDismissRequest = {
+//                setViewModel.closeDiceDialog()
+//            },
+//            confirmButton = { })
+//    }
     if (setUiState.isDiceDialogOpen) {
-        AlertDialog(
-            title = { Text(text = "Dice Menu", fontSize = 20.sp) },
-            text =
-            {
-                Column {
+        Dialog(onDismissRequest = { setViewModel.closeDiceDialog() }) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp)
+
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Dice Menu", fontSize = 20.sp, modifier = Modifier.padding(8.dp))
                     Row {
                         IconButton(onClick = {
-                            setViewModel.addDice(6,"white")
+                            setViewModel.addDice(6, "white")
                         }) {
                             Image(
                                 painter = painterResource(id = R.drawable.dice6_3),
@@ -141,7 +231,7 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                         }
 
                         IconButton(onClick = {
-                            setViewModel.addDice(6,"blue")
+                            setViewModel.addDice(6, "blue")
                         }) {
                             Image(
                                 painter = painterResource(id = R.drawable.dice6_3_blue),
@@ -150,7 +240,7 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                         }
 
                         IconButton(onClick = {
-                            setViewModel.addDice(6,"red")
+                            setViewModel.addDice(6, "red")
                         }) {
                             Image(
                                 painter = painterResource(id = R.drawable.dice6_3_red),
@@ -160,16 +250,17 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                         }
 
                         IconButton(onClick = {
-                            setViewModel.addDice(6,"green")
+                            setViewModel.addDice(6, "green")
                         }) {
                             Image(
                                 painter = painterResource(id = R.drawable.dice6_3_green),
                                 contentDescription = "Green D6 Dice"
                             )
                         }
-
+                    }
+                    Row {
                         IconButton(onClick = {
-                            setViewModel.addDice(4,"D4_white")
+                            setViewModel.addDice(4, "D4_white")
                         }) {
                             Image(
                                 painter = painterResource(id = R.drawable.dice4_3),
@@ -177,11 +268,11 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                             )
                         }
                     }
-                    Row (
+                    Row(
                         modifier = Modifier
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceBetween
-                    ){
+                    ) {
                         TextButton(onClick = { setViewModel.clearDiceSet() }) {
                             Text(text = "Delete All")
                         }
@@ -190,13 +281,8 @@ fun SetUi(set: Int, setViewModel: SetViewModel = viewModel()) {
                         }
                     }
                 }
-
-
-            },
-            onDismissRequest = {
-                setViewModel.closeDiceDialog()
-            },
-            confirmButton = { })
+            }
+        }
     }
 
 
